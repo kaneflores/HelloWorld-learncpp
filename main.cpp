165e52f138243ec9e9c1ee37ac442875fd1f8caa
kaneflores <kaneflores911@gmail.com>
2026-07-14 17:34:13 -0400

    added console close buffer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limtis>
 
 using namespace std;
 
@@ -9,5 +10,8 @@ using namespace std;
 int main(){
     cout << "Hello World" << endl
         << "This worked too" << endl;
+    cin.clear(); // reset any error flags
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a newline
+    cin.get(); // get one more char from the user (waits for user to press enter)
     return 0;
 }
