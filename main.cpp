e9be219ee5ff64fd43b9ba545f988811b127f8f6
kaneflores <kaneflores911@gmail.com>
2026-07-15 19:23:28 -0400

    snippet main created

--- main.cpp
+++ main.cpp
@@ -10,12 +10,17 @@ using namespace std;
 int main(){
     cout << "Hello World" << endl
         << "Apfel" << endl;
-    cin.clear(); // reset any error flags
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a newline
-    cin.get(); // get one more char from the user (waits for user to press enter)
+    cin.clear();                                            // reset any error flags
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');    // ignore any characters in the input buffer until we find a newline
+    cin.get();                                              // get one more char from the user (waits for user to press enter)
     return 0;
     
 }
+
+// int main(){
+//     // Replace with code snippet
+//     return 0;
+// }
 
 
 /*
