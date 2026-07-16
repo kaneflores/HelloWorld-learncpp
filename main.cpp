1cc098d7f0e86d94405b19c84ae92cfed1e1459a
kaneflores <kaneflores911@gmail.com>
2026-07-16 19:22:06 -0400

    quiz snippet for 1.5

--- main.cpp
+++ main.cpp
@@ -24,15 +24,10 @@ programs/projects.
 
 int main()
 {
-    std::cout << "Enter two numbers: ";
-
-    int x{};
-    std::cin >> x;
-
-    int y{};
-    std::cin >> y;
-
-    std::cout << "You entered " << x << " and " << y << '\n';
+    std::cout << "Enter a number: "; // ask user for a number
+    int x{}; // define variable x to hold user input
+    std::cin >> x; // get number from keyboard and store it in variable x
+    std::cout << "You entered " << x << '\n';
 
     return 0;
 }
