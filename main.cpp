4cf1f858c4efb76316d58560d44bfe76ac7839c5
kaneflores <kaneflores911@gmail.com>
2026-07-16 19:31:09 -0400

    quiz completed

--- main.cpp
+++ main.cpp
@@ -24,10 +24,10 @@ programs/projects.
 
 int main()
 {
-    std::cout << "Enter a number: "; // ask user for a number
-    int x{}; // define variable x to hold user input
-    std::cin >> x; // get number from keyboard and store it in variable x
-    std::cout << "You entered " << x << '\n';
+    std::cout << "Enter 3 numbers: "; // ask user for a number
+    int x{}, int y{}, int z{}; // define variable x to hold user input
+    std::cin >> x >> y >> z; // get number from keyboard and store it in variable x
+    std::cout << "You entered " << x << ","<< y << '\n' << z << '\n';
 
     return 0;
 }
