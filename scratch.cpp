8e3b87169ee477a4d1b755c3b8df0b1387f3b651
kaneflores <kaneflores911@gmail.com>
2026-07-19 12:35:47 -0400

    created scratch cpp for testing on my own

---
+++ scratch.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << 4 << ".\n"; /// this is  aliteral
+
+    int x {4};
+    cout << x << '\n'; // print the value of a variable
+    return 0;
+}
