7d8f6b17d551143f0f2778449ff55d4524d0b44e
kaneflores <kaneflores911@gmail.com>
2026-07-21 19:57:12 -0400

    mostly good solution

--- scratch.cpp
+++ scratch.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main(){
     cout << "Enter an integer: \n";
 
-    int num{};      // define variable num as an integer variable
-    cin >> num;     // get integer value from user's keyboard
+    int num{};                                                  // define variable num as an integer variable
+    cin >> num;                                                 // get integer value from user's keyboard
 
-    num = num * 2;
-
-    cout << "Double that number is: " << num << '\n';
+    int doubleNum{ num * 2};                                    // define a new variable and initialize it with num * 2
+    cout << "Double that number is: " << doubleNum << '\n';     // then print the value of that variable here
 
     
 
