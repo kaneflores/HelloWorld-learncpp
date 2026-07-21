5d287b2cc42cc1665872943b6ad5c15666e49f63
kaneflores <kaneflores911@gmail.com>
2026-07-21 19:47:54 -0400

    worst solution

--- scratch.cpp
+++ scratch.cpp
@@ -7,6 +7,11 @@ int main(){
 
     int num{};      // define variable num as an integer variable
     cin >> num;     // get integer value from user's keyboard
+
+    num = num * 2;
+
+    cout << "Double that number is: " << num << '\n';
+
     
 
     return 0;
