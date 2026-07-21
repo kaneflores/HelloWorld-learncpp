116fcede9dc15f8d622fb20b40969e2f2a0fb2a0
kaneflores <kaneflores911@gmail.com>
2026-07-21 19:43:55 -0400

    cp

--- scratch.cpp
+++ scratch.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main(){
     cout << "Enter an integer: \n";
+
+    int num{};      // define variable num as an integer variable
+    cin >> num;     // get integer value from user's keyboard
     
 
     return 0;
