df2e8611bcfafe82dcb20d868b023b4c8227d3e0
kaneflores <kaneflores911@gmail.com>
2026-07-21 19:58:03 -0400

    preferred soltuion

--- scratch.cpp
+++ scratch.cpp
@@ -8,8 +8,7 @@ int main(){
     int num{};                                                  // define variable num as an integer variable
     cin >> num;                                                 // get integer value from user's keyboard
 
-    int doubleNum{ num * 2};                                    // define a new variable and initialize it with num * 2
-    cout << "Double that number is: " << doubleNum << '\n';     // then print the value of that variable here
+    cout << "Double that number is: " << num * 2 << '\n';     // then print the value of that variable here
 
     
 
