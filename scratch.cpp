32cbb5d6cb331848b78fdc2fdfc9d5e54007ed5d
kaneflores <kaneflores911@gmail.com>
2026-07-22 19:57:07 -0400

    cp

--- scratch.cpp
+++ scratch.cpp
@@ -2,22 +2,8 @@
 
 using namespace std;
 
-int getValueFromUser(){
-    cout << "Enter an integer: ";
-    int num{ };
-    cin >> num;
-    return num;
-}
 
-void doubleNumber(int x){
-    cout << "Doubled number of " << x << " is: " << x * 2 << '\n';
-}
 int main(){
 
-    int value{getValueFromUser()};
-    doubleNumber(value);
-
-    
-    return 0;
 }
 
