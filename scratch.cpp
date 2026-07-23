70ca1de4c156db864f8eb5cc1e80208b3a9cf5ae
kaneflores <kaneflores911@gmail.com>
2026-07-22 20:56:47 -0400

    messed around with tasks and coding

--- scratch.cpp
+++ scratch.cpp
@@ -1,7 +1,11 @@
 #include <iostream> // lib for basic input and output
-
 using namespace std;
 
-
+int add(int x, int y);
 int main(){
+    int x{1};
+    int y{20};
+
+    cout << add(x,y) << '\n';
+    return 0;
 }
