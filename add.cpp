a229ebef0e63d44569b15968ba2a26ce53daade1
kaneflores <kaneflores911@gmail.com>
2026-07-23 22:18:11 -0400

    messing around again with preprocessor directives

--- add.cpp
+++ add.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
-int add(int x, int y){
-    return x + y;
+#define PRINT
+void doSomething()
+{
+#ifdef PRINT
+    std::cout << "Printing!\n";
+#endif
+#ifndef PRINT
+    std::cout << "Not printing!\n";
+#endif
 }
