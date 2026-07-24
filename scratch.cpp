b6455cbca7821edac5f8062a603781168e773a3c
kaneflores <kaneflores911@gmail.com>
2026-07-23 22:09:17 -0400

    preprocessor directive messing aroind

--- scratch.cpp
+++ scratch.cpp
@@ -1,6 +1,10 @@
 #include <iostream> // lib for basic input and output
+#define FOO 
 
 int add(int x, int y);
 int main(){
+    #ifdef FOO
+    std::cout << FOO << '\n';
+    #endif
     return 0;
 }
