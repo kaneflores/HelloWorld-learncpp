86e3bc9557890a2be36521953dd80a071dac12ae
kaneflores <kaneflores911@gmail.com>
2026-07-25 12:34:41 -0400

    fixed files

--- add.cpp
+++ add.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#define PRINT
-void doSomething()
-{
-#ifdef PRINT
-    std::cout << "Printing!\n";
-#endif
-#ifndef PRINT
-    std::cout << "Not printing!\n";
-#endif
+
+int add(int x, int y){
+    return x + y;
 }
--- scratch.cpp
+++ scratch.cpp
@@ -1,10 +1,10 @@
-#include <iostream> // lib for basic input and output
-#define FOO 
+// #include <iostream> // lib for basic input and output
+// #define FOO 2
 
-int add(int x, int y);
-int main(){
-    #ifdef FOO
-    std::cout << FOO << '\n';
-    #endif
-    return 0;
-}
+// int add(int x, int y);
+// int main(){
+// #ifdef FOO
+//     std::cout << FOO << '\n';  
+// #endif
+//     return 0;
+// }
