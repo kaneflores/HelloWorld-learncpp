241d76ca4a74748bcba22ecfa98c30cfe6cafc36
kaneflores <kaneflores911@gmail.com>
2026-08-06 18:46:40 -0400

    cp

--- scratch.cpp
+++ scratch.cpp
@@ -8,18 +8,18 @@
 // #endif
 //     return 0;
 // }
-#include <iostream>
+// #include <iostream>
 
-void printValue(int value)
-{
-    std::cout << value << '\n';
-}
+// void printValue(int value)
+// {
+//     std::cout << value << '\n';
+// }
 
-int main()
-{
-    printValue(5);
-    printValue(6);
-    printValue(7);
+// int main()
+// {
+//     printValue(5);
+//     printValue(6);
+//     printValue(7);
 
-    return 0;
-}
+//     return 0;
+// }
