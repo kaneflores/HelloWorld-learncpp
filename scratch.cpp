bc281ea6425cff01c498fbb3f789b1137b80f1c2
kaneflores <kaneflores911@gmail.com>
2026-08-07 18:34:27 -0400

    cp

--- scratch.cpp
+++ scratch.cpp
@@ -1,38 +1,38 @@
-// #include <iostream> // lib for basic input and output
-// #define FOO 2
+// // #include <iostream> // lib for basic input and output
+// // #define FOO 2
 
-// int add(int x, int y);
-// int main(){
-// #ifdef FOO
-//     std::cout << FOO << '\n';  
-// #endif
-//     return 0;
+// // int add(int x, int y);
+// // int main(){
+// // #ifdef FOO
+// //     std::cout << FOO << '\n';  
+// // #endif
+// //     return 0;
+// // }
+// #include <iostream>
+
+// int readNumber()
+// {
+// 	std::cout << "Please enter a number: ";
+// 	int x {};
+// 	std::cin >> x;
+// 	return x;
 // }
-#include <iostream>
 
-int readNumber()
-{
-	std::cout << "Please enter a number: ";
-	int x {};
-	std::cin >> x;
-	return x;
-}
+// void writeAnswer(int x)
+// {
+// 	std::cout << "The quotient is: " << x << '\n';
+// }
 
-void writeAnswer(int x)
-{
-	std::cout << "The quotient is: " << x << '\n';
-}
+// int main()
+// {
+// 	int x{ };
+// 	int y{ };
+// 	x = readNumber();
+// 	x = readNumber();
+//     writeAnswer(x+y);
+// 	writeAnswer(x/y);
 
-int main()
-{
-	int x{ };
-	int y{ };
-	x = readNumber();
-	x = readNumber();
-    writeAnswer(x+y);
-	writeAnswer(x/y);
-
-	return 0;
-}
+// 	return 0;
+// }
 
 
