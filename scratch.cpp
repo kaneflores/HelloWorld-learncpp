9a18ac79766f47f38819066ba5fe38f9aea94934
kaneflores <kaneflores911@gmail.com>
2026-08-11 21:43:18 -0400

    test

--- scratch.cpp
+++ scratch.cpp
@@ -55,12 +55,14 @@
 //     return 0;
 // }
 #include <iostream>
-#include <limits>
 
 int main()
 {
-    std::cout << std::boolalpha; // print bool as true or false rather than 1 or 0
-    std::cout << "float: " << std::numeric_limits<float>::is_iec559 << '\n';
-    std::cout << "double: " << std::numeric_limits<double>::is_iec559 << '\n';
-    std::cout << "long double: " << std::numeric_limits<long double>::is_iec559 << '\n';
+    std::cout << std::boolalpha;
+	bool b{}; // default initialize to false
+	std::cout << "Enter a boolean value: ";
+	std::cin >> b;
+	std::cout << "You entered: " << b << '\n';
+
+	return 0;
 }
