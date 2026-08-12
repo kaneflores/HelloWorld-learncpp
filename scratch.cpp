dc9eebf7933f7ced06affc93d825badd8db9f488
kaneflores <kaneflores911@gmail.com>
2026-08-12 15:37:11 -0400

    quiz

--- scratch.cpp
+++ scratch.cpp
@@ -67,3 +67,19 @@
 // 	return 0;
 // }
 
+#include <iostream>
+
+int main(){
+    std::cout << "Enter an integer: ";
+    int x {};
+    std::cin >> x;
+
+    if (x == 0){
+        std::cout << "The value is zero\n";
+    
+    }
+    else{
+        std::cout << "The value is non-zero\n";
+    }
+    return 0;
+}
