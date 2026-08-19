17f5e8a546ff73db0a6364199d1260b5ff32a05b
kaneflores <kaneflores911@gmail.com>
2026-08-19 16:39:54 -0400

    cp

--- scratch.cpp
+++ scratch.cpp
@@ -8,13 +8,6 @@ using namespace std::string_view_literals;
 int main(){
     std::cout << "Hello, world!\n"sv;
     
-    constexpr int x{ 7 };
-    constexpr int y{ 4 };
-
-    std::cout << "int / int = " << x / y << '\n';
-    std::cout << "double / int = " <<x / static_cast<double>(y) << '\n';
-    std::cout << "int / double = " << x / static_cast<double>(y) << '\n';
-    std::cout << "double / double = " << static_cast<double>(x) / static_cast<double>(y) << '\n';
 
 
 }
