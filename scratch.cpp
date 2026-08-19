46eeb00f7d8f4e6b3a44b8ab277dac350a7c9df1
kaneflores <kaneflores911@gmail.com>
2026-08-19 16:49:59 -0400

    messing around

--- scratch.cpp
+++ scratch.cpp
@@ -7,7 +7,12 @@ using namespace std::string_view_literals;
 
 int main(){
     std::cout << "Hello, world!\n"sv;
-    
+    int x{ 1 };
+    int y{ 2 };
+
+    std::cout << (++x, ++y) << '\n'; // increment x and y, evaluates to the right operand
+
+    return 0;
 
 
 }
