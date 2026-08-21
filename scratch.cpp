7a7e947220e72163970cfc34fcdf9dd6a9b18fdb
kaneflores <kaneflores911@gmail.com>
2026-08-20 20:10:58 -0400

    cp

--- scratch.cpp
+++ scratch.cpp
@@ -10,34 +10,7 @@ int getValue(){
     return x;
 }
 
-std::string_view getQuantityPhrase(int num){
-    if (num < 0)
-        return "negative";
-    else if (num == 0)
-        return "no";
-    else if (num == 1)
-        return "a single";
-    else if (num == 2)
-        return "a couple of";
-    else if (num == 3)
-        return "a few";
-    return "a lot";
-}
-
-std::string_view getApplesPluralized(int x){
-    return (x == 1) ? "apple" : "apples";
-}
-
 int main(){
-    constexpr int maryApples { 3 };
-    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
-
-    std::cout << "How many apples do you have? ";
-    int numApples{};
-    std::cin >> numApples;
-
-    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
-
-    return 0;
+    
     return 0;
 }
