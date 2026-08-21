be23f64f385410b5cc4f98df29260c642a27b4c1
kaneflores <kaneflores911@gmail.com>
2026-08-21 14:12:41 -0400

    quzzing 0.2

--- scratch.cpp
+++ scratch.cpp
@@ -13,9 +13,5 @@ int getValue(){
 
 int main(){
     std::bitset<4> x {0b1100};
-
-    std::cout << x << '\n';
-    std::cout << (x>>1) << '\n';    // shift right by 1, yields 0110
-    std::cout << (x << 1) << '\n';  // shift left by 1, yielding 1000;
     return 0;
 }
