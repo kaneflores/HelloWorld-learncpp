1abcc489d71b3045801d95e60cbe54c8e5058f37
kaneflores <kaneflores911@gmail.com>
2026-08-20 20:10:00 -0400

    quizzing

--- scratch.cpp
+++ scratch.cpp
@@ -10,20 +10,34 @@ int getValue(){
     return x;
 }
 
-bool isEven(int x){
-    return !(x %2);//!()
+std::string_view getQuantityPhrase(int num){
+    if (num < 0)
+        return "negative";
+    else if (num == 0)
+        return "no";
+    else if (num == 1)
+        return "a single";
+    else if (num == 2)
+        return "a couple of";
+    else if (num == 3)
+        return "a few";
+    return "many";
+}
+
+std::string_view getApplesPluralized(int x){
+    return (x == 1) ? "apple" : "apples";
 }
 
 int main(){
-    std::cout << "Enter an integer: ";
-    int x{};
-    std::cin >> x;
+    constexpr int maryApples { 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
 
-    if (isEven(x)){
-        std::cout << x << " is even\n";
-    }
-    else{
-        std::cout << x << " is odd\n";
-    }
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
+    return 0;
     return 0;
 }
