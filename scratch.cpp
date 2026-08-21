61dc150b6a9db82b10517e7fa78fc7ab01cc2082
kaneflores <kaneflores911@gmail.com>
2026-08-21 13:39:44 -0400

    cp O.1

--- scratch.cpp
+++ scratch.cpp
@@ -12,20 +12,6 @@ int getValue(){
 }
 
 int main(){
-    std::bitset<0> bits{ 0 };
-    if (bits.size()>1 || bits.size() == 0){
-        std::cout << bits.size() << " bits are in the bitset\n";
-    }else{
-        std::cout << bits.size() << " bit is in the bitset\n";
-    }
-    
-    std::cout << bits.count() << " bits are set to true\n";
-
-    std::cout << std::boolalpha;
-    std::cout << "All bits are true: " << bits.all() << '\n';
-    std::cout << "Some bits are true: " << bits.any() << '\n';
-    std::cout << "No bits are true: " << bits.none() << '\n';
-
     
     return 0;
 }
