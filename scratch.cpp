bd1ed64316023e094b3e1385e34257ea6e7884c0
kaneflores <kaneflores911@gmail.com>
2026-08-21 13:33:07 -0400

    messing aorund

--- scratch.cpp
+++ scratch.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string_view>
 #include <cstdint> // for std::uint8_t
+#include <bitset>
 using namespace std::string_view_literals;
 
 int getValue(){
@@ -11,7 +12,14 @@ int getValue(){
 }
 
 int main(){
+    std::bitset<8> bits{0b0000'0101}; // we need 8 bits, start with bit pattern 0000 0101
+    //bits.set(3);    // set bit position 3 to 1 ( now we have 0000 1101)
+    bits.flip(4);   // flip bit 4 (now 0001 1101)
+    bits.reset(4);  // set bit 4 back to 0 ( now its 0000 1101)
 
+    std::cout << "All the bits: " << bits << '\n';
+    std::cout << "Bit 3 has value: " << bits.test(3) << '\n';
+    std::cout << "Bit 4 has value: " << bits.test(4) << '\n';
 
     
     return 0;
