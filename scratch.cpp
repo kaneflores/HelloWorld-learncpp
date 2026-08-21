65cc1fdeb2c7126e60ff6d69f8c68a51040e7b92
kaneflores <kaneflores911@gmail.com>
2026-08-21 14:53:15 -0400

    q2done

--- scratch.cpp
+++ scratch.cpp
@@ -11,13 +11,19 @@ int getValue(){
     return x;
 }
 
+std::bitset<4> rotl(std::bitset<4> bits){
+    
+    return (bits << 1) | (bits >> 3);
+}
 int main(){
-    std::bitset<4> x {0b1100};
+    std::bitset<4> bits1 {0b0001};
+    std::cout << rotl(bits1) <<'\n';
 
+    std::bitset<4> bits2{0b1001};
+    std::cout << rotl(bits2) << '\n';
 
+    std::bitset<4> bits3{0b1111}; 
+    std::cout << rotl(bits3) << '\n';
 
-    0111 &
-    1001
-    0001
     return 0;
 }
