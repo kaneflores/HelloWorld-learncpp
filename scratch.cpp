6e1993186e6f5ff2a25b5f47f441088757566b6a
kaneflores <kaneflores911@gmail.com>
2026-08-29 18:03:55 -0400

    8.2 cp

--- scratch.cpp
+++ scratch.cpp
@@ -28,16 +28,6 @@ namespace constants{
 int main(){
     
 
-    std::cout << "Enter your height (in cm): ";
-    int x{};
-    std::cin >> x;
-
-    if (x >= constants::minRideHeightCM){
-        std::cout << "You are tall enough to ride.\n";
-    }else{
-        std::cout << "You are not tall enough to ride. \n";
-        std::cout << "Too bad\n";
-    }
 
 
 
