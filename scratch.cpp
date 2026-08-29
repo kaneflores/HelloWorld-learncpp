76ec08e8c7ae4ee1ea2696d5528ce3cbc689d3fa
kaneflores <kaneflores911@gmail.com>
2026-08-29 14:46:39 -0400

    8.1 cp

--- scratch.cpp
+++ scratch.cpp
@@ -24,11 +24,12 @@ std::bitset<4> rotl(std::bitset<4> bits){
 
 int main(){
     
-    std::cout << accumulate(4) << '\n'; // prints 4 etc
-    std::cout << accumulate(3) << '\n'; // prints 7
-    std::cout << accumulate(2) << '\n'; // 9
-    std::cout << accumulate(1) << '\n'; // 10
-    std::cout << accumulate(0) << '\n'; //10
+    std::cout << "Enter an integer: ";
+
+    int x {};
+    std::cin >> x;
+
+    std::cout << "You entered " << x << '\n';
     return 0;
 }
 
