acf6479569c5294dd42d6a696be7c670de586009
kaneflores <kaneflores911@gmail.com>
2026-08-29 14:40:20 -0400

    quizzed 3

--- scratch.cpp
+++ scratch.cpp
@@ -20,30 +20,17 @@ std::bitset<4> rotl(std::bitset<4> bits){
     
     return (bits << 1) | (bits >> 3);
 }
+
+
 int main(){
-    std::cout << "Enter a positive number: ";
-	int num{};
-	std::cin >> num;
-
-
-	if (num < 0)
-		std::cout << "Negative number entered.  Making positive.\n";
     
-	std::cout << "You entered: " << num;
-
-	return 0;
-    
-
+    std::cout << accumulate(4) << '\n'; // prints 4 etc
+    std::cout << accumulate(3) << '\n'; // prints 7
+    std::cout << accumulate(2) << '\n'; // 9
+    std::cout << accumulate(1) << '\n'; // 10
+    std::cout << accumulate(0) << '\n'; //10
     return 0;
 }
 
-/////////////////////////////
-// #ifndef CONSTANTS_H
-// #define CONSTANST_H
 
-// namespace Constants{
-//     inline constexpr int maxClassSize {35}; // only remove inline keyword if not cpp17 capable.
-// }
-
-// #endif
 
