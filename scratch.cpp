e8d5470b6551aeb5b160d2c27c021438f3c7d793
kaneflores <kaneflores911@gmail.com>
2026-08-29 14:29:28 -0400

    qu izzed q1

--- scratch.cpp
+++ scratch.cpp
@@ -21,9 +21,19 @@ std::bitset<4> rotl(std::bitset<4> bits){
     return (bits << 1) | (bits >> 3);
 }
 int main(){
+    std::cout << "Enter a positive number: ";
+	int num{};
+	std::cin >> num;
 
+
+	if (num < 0)
+		std::cout << "Negative number entered.  Making positive.\n";
     
+	std::cout << "You entered: " << num;
+
+	return 0;
     
+
     return 0;
 }
 
