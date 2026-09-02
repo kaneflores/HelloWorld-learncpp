f5bdf538baf1938c896e1f0a20a690af057d19fd
kaneflores <kaneflores911@gmail.com>
2026-09-01 20:48:55 -0400

    my turn

--- scratch.cpp
+++ scratch.cpp
@@ -62,9 +62,21 @@ void foo(int x, int y){
 //     }
 int main(){
     
-    
+    int outer{5};
+    while(outer>=1){
 
-    
+        int inner{outer};
+        while(inner>=1){
+            std::cout << inner << " ";
+            --inner;
+        }
+
+        std::cout << '\n';
+        --outer;
+
+
+    }
+
     return 0;
 }
 
