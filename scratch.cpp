2885c06575d93dd9874cb59cc76db8920477ef8b
kaneflores <kaneflores911@gmail.com>
2026-09-01 20:32:51 -0400

    q2

--- scratch.cpp
+++ scratch.cpp
@@ -62,8 +62,12 @@ void foo(int x, int y){
 //     }
 int main(){
     
+    char myChar {'a'};
+    while (myChar <= 'z'){
+        std::cout << myChar << ' ' << static_cast<int>(myChar) << '\n';
+        ++myChar;
+    }
 
-    
     return 0;
 }
 
