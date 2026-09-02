0a988e1e74489d6d07b557807d0f8e59727c64eb
kaneflores <kaneflores911@gmail.com>
2026-09-01 20:49:18 -0400

    cp 8.8

--- scratch.cpp
+++ scratch.cpp
@@ -62,20 +62,7 @@ void foo(int x, int y){
 //     }
 int main(){
     
-    int outer{5};
-    while(outer>=1){
-
-        int inner{outer};
-        while(inner>=1){
-            std::cout << inner << " ";
-            --inner;
-        }
-
-        std::cout << '\n';
-        --outer;
-
-
-    }
+    
 
     return 0;
 }
