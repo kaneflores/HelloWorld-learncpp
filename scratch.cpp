1484aaec5b1fbf03bd71d676b6fda2f4691322bb
kaneflores <kaneflores911@gmail.com>
2026-09-03 18:36:20 -0400

    cp 8.13

--- scratch.cpp
+++ scratch.cpp
@@ -61,22 +61,6 @@ void foo(int x, int y){
 //         ++yr;
 //     }
 int main(){
-    int count{ 0 }; // count how many times the loop iterates
-    bool keepLooping { true }; // controls whether the loop ends or not
-    while (keepLooping)
-    {
-        std::cout << "Enter 'e' to exit this loop or any other character to continue: ";
-        char ch{};
-        std::cin >> ch;
-
-        if (ch == 'e')
-            keepLooping = false;
-        else
-        {
-            ++count;
-            std::cout << "We've iterated " << count << " times\n";
-        }
-    }
     
     return 0;
 }
