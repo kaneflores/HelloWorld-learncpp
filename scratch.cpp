97cdadf686d20f87bef397dd4f7363a95ae31a1b
kaneflores <kaneflores911@gmail.com>
2026-09-11 16:33:09 -0400

    cp 11.10

--- scratch.cpp
+++ scratch.cpp
@@ -75,27 +75,4 @@ int main(){
     return 0;
 }
 
-template <int N>
-constexpr int factorial(){
-    static_assert(N >= 0, "factorial(): N must be non-negative");
 
-    int product {1};
-    for (int i {2}; i <= N; ++i){
-        product *= i;
-    }
-    return product;
-}
-
-
-// define your factorial() function template here
-
-int main()
-{
-    static_assert(factorial<0>() == 1);
-    static_assert(factorial<3>() == 6);
-    static_assert(factorial<5>() == 120);
-
-    factorial<-3>(); // should fail to compile
-
-    return 0;
-}
